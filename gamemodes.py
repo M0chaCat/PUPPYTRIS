8b515af198bb83871028409a5ac84a774c5ca4a1
Sarah <77510299+cringeybabey@users.noreply.github.com>
2025-12-02 16:59:59 -0700

    Fixed crash on piece inversion

--- gamemodes.py
+++ gamemodes.py
@@ -1,16 +1,15 @@
-import pieces, skinloader
+import pieces
 
 class pentomino_base():
     pieces_dict = pieces.penta_dict
     piece_types = pieces.PIECE_TYPES_PENTA
-    piece_inversions = pieces.TETRA_INVERSIONS
+    piece_inversions = pieces.PENTA_INVERSIONS
     hold_pieces_count = 2
     piece_size = 5
     
 class tetramino_base():
     pieces_dict = pieces.tetra_dict
     piece_types = pieces.PIECE_TYPES_TETRA
-    piece_inversions = pieces.PENTA_INVERSIONS
+    piece_inversions = pieces.TETRA_INVERSIONS
     hold_pieces_count = 1   
     piece_size = 4
-    
